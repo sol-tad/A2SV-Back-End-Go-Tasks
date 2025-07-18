7687a8eaa545cbbee1ae5a1905d01ed8879fbfd1
sol-tad <soltad65@gmail.com>
2025-07-18 15:54:31 +0300

    update task management api by integrating mongodb and Task 5 completed

---
+++ Task 5/task_manager/config/db.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var TaskCollection *mongo.Collection
+
+func ConnectDB(){
+	client,err:=mongo.NewClient(options.Client().ApplyURI("mongodb+srv://soltad65:12341234@taskmanagementcluster.tefktkx.mongodb.net/?retryWrites=true&w=majority&appName=TaskManagementCluster"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx,cancel:=context.WithTimeout(context.Background(),10*time.Second)
+	defer cancel()
+	err=client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	TaskCollection=client.Database("taskdb").Collection("tasks")
+	log.Println("Connected to MongoDB")
+
+}
---
+++ Task 5/task_manager/controllers/task_controller.go
@@ -0,0 +1,68 @@
+package controllers
+
+import (
+	"net/http"
+	"task_manager/data"
+	"task_manager/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetTasks(c *gin.Context) {
+	tasks, err := data.GetAllTasks()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get tasks"})
+		return
+	}
+	c.JSON(http.StatusOK, tasks)
+}
+
+func GetTaskByID(c *gin.Context) {
+	id := c.Param("id")
+	task, err := data.GetTaskByID(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+	c.JSON(http.StatusOK, task)
+}
+
+func CreateTask(c *gin.Context) {
+	var task models.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	newTask, err := data.CreateTask(task)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create task"})
+		return
+	}
+	c.JSON(http.StatusCreated, newTask)
+}
+
+func UpdateTask(c *gin.Context) {
+	id := c.Param("id")
+	var task models.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatedTask, err := data.UpdateTask(id, task)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+	c.JSON(http.StatusOK, updatedTask)
+}
+
+func DeleteTask(c *gin.Context) {
+	id := c.Param("id")
+	if err := data.DeleteTask(id); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Deleted successfully"})
+}
